be7e8fffcab287bf3009fb9f2293eb220673ec19
Mahendra368 <jaldumahendrasatya@gmail.com>
2026-02-15 10:23:40 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PincodeFetcher from './components/picodeFetcher'
+
+const App = () => {
+  return (
+   <>
+    <PincodeFetcher />
+   </>
+  )
+}
+
+export default App
